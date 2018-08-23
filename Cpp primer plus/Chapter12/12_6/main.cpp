9646ed746a13ba9044b14e2b72bf219673a02801
Achilles-10 <1029991674@qq.com>
2018-08-23 15:26:37 +0800

    add 12_6

---
+++ Cpp primer plus/Chapter12/12_6/main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "queue.h"
+
+const int MIN_PER_HR = 60;
+
+bool newcustomer(double x);
+
+int main() {
+	using namespace std;
+
+	srand(time(0));
+
+	cout << "Case Study: Bank of Heather Automatic Teller\n";
+	cout << "Enter maximum size of queue: ";
+	int qs;
+	cin >> qs;
+	Queue line1(qs), line2(qs);
+
+	cout << "Enter the number of simulation hours: ";
+	int hours;
+	cin >> hours;
+
+	long cycle_limit = MIN_PER_HR * hours;
+
+	cout << "Enter the average number of customer per hour: ";
+	double perhour;
+	cin >> perhour;
+	double min_per_cost;
+	min_per_cost = MIN_PER_HR / perhour;
+
+	Item temp;
+	long turnaways = 0;
+	long customers = 0;
+	long served = 0;
+	long sum_line1 = 0, sum_line2 = 0;
+	int wait_time1 = 0, wait_time2 = 0;
+	long line_wait = 0;
+
+	for (int cycle = 0; cycle < cycle_limit; cycle++) {
+		if (newcustomer(min_per_cost)) {
+			if (line1.isfull() && line2.isfull())
+				turnaways++;
+			else {
+				customers++;
+				temp.set(cycle);
+				if (line1.queuecount() < line2.queuecount())
+					line1.enqueue(temp);
+				else
+					line2.enqueue(temp);
+			}
+		}
+		if (wait_time1 <= 0 && !line1.isempty()) {
+			line1.dequeue(temp);
+			wait_time1 = temp.ptime();
+			line_wait += cycle - temp.when();
+			served++;
+		}
+		if (wait_time2 <= 0 && !line2.isempty()) {
+			line2.dequeue(temp);
+			wait_time2 = temp.ptime();
+			line_wait += cycle - temp.when();
+			served++;
+		}
+		if (wait_time1 > 0)
+			wait_time1--;
+		if (wait_time2 > 0)
+			wait_time2--;
+		sum_line1 += line1.queuecount();
+		sum_line2 += line2.queuecount();
+	}
+
+	if (customers > 0) {
+		cout << "customers accepted: " << customers << endl;
+		cout << "  customers served: " << served << endl;
+		cout << "         turnaways: " << turnaways << endl;
+		cout << "average queue size: ";
+		cout.precision(2);
+		cout.setf(ios_base::fixed, ios_base::floatfield);
+		cout << "Q1: " << (double) sum_line1 / cycle_limit << endl
+		     << "\t\t    Q2: " << (double) sum_line2 / cycle_limit << endl;
+		cout << " average wait time: " << (double) line_wait / served << " minutes\n";
+	}
+	else
+		cout << "No customers!\n";
+	cout << "Done!\n";
+
+	return 0;
+
+}
+
+bool newcustomer(double x) {
+	return (rand() * x / RAND_MAX < 1);
+}
