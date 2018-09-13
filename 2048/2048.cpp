6574e319f4c534611c69d897bca5fa21afe4d80d
Achilles-10 <1029991674@qq.com>
2018-09-13 19:19:52 +0800

    add 2048

--- 2048/2048.cpp
+++ 2048/2048.cpp
@@ -5,10 +5,172 @@
 
 using namespace std;
 
-string help[4] = {};
-enum {Title,Operation,Tips};
-
-
 void Board::Draw() const {
+	string help[4] = {};
+	enum {
+		Title, Operation, Tips
+	};
+}
 
+int Board::RandomNum() const {
+	int temp = rand() % 100 + 1;
+	if (temp > 80)
+		return 4;
+	else
+		return 2;
+}
+
+void Board::AddRandomNum() {
+	int x, y;
+	int count = 0;
+	int index[Size * Size][2];
+
+	for (x = 0; x < Size; x++) {
+		for (y = 0; y < Size; y++) {
+			if (!Table_Now[x][y].GetState()) {
+				index[count][0] = x;
+				index[count][1] = y;
+				count++;
+			}
+		}
+	}
+
+	if (count > 0) {
+		int num = rand() % count;
+		x = index[num][0];
+		y = index[num][1];
+		Table_Now[x][y].SetValue(RandomNum());
+		Table_Now[x][y].SetState();
+	}
+}
+
+void Board::SetUpBoard() {
+	AddRandomNum();
+	AddRandomNum();
+	Draw();
+}
+
+void Board::RestartGame() {
+	turn = 0;
+	for (int i = 0; i < Size; ++i) {
+		for (int j = 0; j < Size; ++j) {
+			Table_Now[i][j].UnSetState();
+			Table_Now[i][j].SetValue(0);
+		}
+	}
+	SetUpBoard();
+}
+
+void Board::RowInvert(int row) {
+	Tile temp;
+	for (int column = 0; column < Size / 2; ++column) {
+		temp = Table_Now[row][column];
+		Table_Now[row][column] = Table_Now[row][Size - column - 1];
+		Table_Now[row][Size - column - 1] = temp;
+	}
+}
+
+void Board::MatrixInvert() {
+	for (int i = 0; i < Size; ++i)
+		RowInvert(i);
+}
+
+void Board::MatrixTransform() {
+	Tile temp;
+	for (int i = 0; i < Size - 1; ++i) {
+		for (int j = i + 1; j < Size; ++j) {
+			temp = Table_Now[i][j];
+			Table_Now[i][j] = Table_Now[j][i];
+			Table_Now[j][i] = temp;
+		}
+	}
+}
+
+void Board::RowTightn(int row) {
+	int i, j, count = 0;
+	int new_row[Size] = {0};
+	for (int column = 0; column < Size; ++column) {
+		if (Table_Now[row][column].GetState())
+			new_row[count++] = Table_Now[row][column].GetValue();
+	}
+	for (j = 0; j < count; ++j) {
+		Table_Now[row][j].SetValue(new_row[j]);
+		Table_Now[row][j].SetState();
+	}
+	for (i = j; i < Size; ++i) {
+		Table_Now[row][i].UnSetState();
+	}
+}
+
+void Board::RowMerge(int row) {
+	int new_row[Size] = {0};
+	bool pair = false;
+	for (int i = 0; i < Size; ++i) {
+		if (pair) {
+			new_row[i] = Table_Now[row][i].GetValue() * 2;
+			pair = false;
+		}
+		else {
+			if (Table_Now[row][i].GetValue() == Table_Now[row][i + 1].GetValue()) {
+				pair = true;
+				new_row[i] = 0;
+			}
+			else
+				new_row[i] = Table_Now[row][i].GetValue();
+		}
+	}
+	for (int j = 0; j < Size; ++j) {
+		if (new_row[j] == 0)
+			Table_Now[row][j].UnSetState();
+		else {
+			Table_Now[row][j].SetValue(new_row[j]);
+			Table_Now[row][j].SetState();
+		}
+	}
+}
+
+void Board::MatrixTighten() {
+	for (int row = 0; row < Size; ++row)
+		RowTightn(row);
+}
+
+void Board::MatrixMerge() {
+	for (int row = 0; row < Size; ++row) {
+		RowTightn(row);
+		RowMerge(row);
+		RowTightn(row);
+	}
+}
+
+bool Board::CanMoveRow(int row) const {
+	for (int i = 0; i < Size; ++i) {
+		if (!Table_Now[row][i].GetState() && Table_Now[row][i + 1].GetState())
+			return true;
+		else if (Table_Now[row][i].GetState() &&
+		         Table_Now[row][i].GetValue() == Table_Now[row][i + 1].GetValue())
+			return true;
+	}
+	return false;
+}
+
+bool Board::CanMoveMatrix() const {
+	for (int row = 0; row < Size; ++row) {
+		if (CanMoveRow(row))
+			return true;
+	}
+	return false;
+}
+
+bool Board::IsLose() const {
+	return !CanMoveMatrix();
+}
+
+bool Board::IsWin() const {
+	for (int i = 0; i < Size; ++i) {
+		for (int j = 0; j < Size; ++j) {
+			if (Table_Now[i][j].GetValue() == 2048)
+				return true;
+		}
+	}
+	return false;
 }
--- 2048/2048.h
+++ 2048/2048.h
@@ -1,24 +1,32 @@
 #ifndef INC_2048_H
 #define INC_2048_H
 
-class Board{
+class Board {
 private:
-	enum {Size = 4};
-	class Tile{        //小格
+	enum {
+		Size = 4
+	};
+
+	class Tile {        //小格
 	private:
 		int value;
 		bool state;
 	public:
-		Tile():state(false),value(0){}
-		explicit Tile(int num):state(true),value(num){}
-		void UnSetState() {state= false;}
-		void SetState(){state= true;}
-		void SetValue(int num){value=num;}
-		bool GetState() const { return state;}
-		int GetValu() const { return value;}
+		Tile() : state(false), value(0) {}
+
+		explicit Tile(int num) : state(true), value(num) {}
+
+		void UnSetState() { state = false; }
+
+		void SetState() { state = true; }
+
+		void SetValue(int num) { value = num; }
+
+		bool GetState() const { return state; }
+
+		int GetValue() const { return value; }
 	};
-	int score;
-	int best;
+
 	int turn;        //操作次数
 
 	Tile Table_Now[Size][Size];
@@ -26,36 +34,55 @@ private:
 	//Tile Table_Prev[Size][Size];
 	//Tile Table_Next[Size][Size];
 
-	int RandomNum() const ;
+	int RandomNum() const;
+
 	void AddRandomNum();
 
 	void SetUpBoard();
-	void Draw() const ;
-	void CopyBoard();
+
+	void Draw() const;
+
+	//void CopyBoard();
+
 	void GameEnd();
 
 	void MoveLeftRow();
+
 	void MoveRightRow();
+
 	void MoveUpRow();
+
 	void MoveDownRow();
+
+	void RowInvert(int row);
+	void RowTightn(int row);
+	void RowMerge(int row);
 
 	void MatrixTransform();//矩阵转置
 	void MatrixInvert();//矩阵颠倒
+	void MatrixTighten();//矩阵靠紧
 	void MatrixMerge();//矩阵合并
 
-	bool CanMoveRow() const ;
-	bool CanMoveMatrix() const ;
+	bool CanMoveRow(int row) const;
 
-	bool IsWin() const ;
-	bool IsLose() const ;
+	bool CanMoveMatrix() const;
+
+	bool IsWin() const;
+
+	bool IsLose() const;
 
 
 public:
 	Board();
+
 	void MoveLeftMatirx();
+
 	void MoveRightMatrix();
+
 	void MoveUpMatrix();
+
 	void MoveDownMatrix();
+
 	void RestartGame();
 	//添加后退前进操作
 	//void LastMatrix();
@@ -63,18 +90,20 @@ public:
 };
 
 
-class Manager:public Board{
+class Manager : public Board {
 public:
 	void MoveLeft();
+
 	void MoveRight();
+
 	void MoveUp();
+
 	void MoveDown();
+
 	void Restart();
 	//void Forward();
 	//void Back();
 };
-
-
 
 
 #endif //INC_2048_H
