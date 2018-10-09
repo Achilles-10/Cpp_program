5f027018bbdc4859b5cea2d00e4693f1c43ef8ef
Achilles-10 <1029991674@qq.com>
2018-10-09 23:42:30 +0800

    add Chapter 16

--- Cpp primer plus/Chapter16/16_8.cpp
+++ Cpp primer plus/Chapter16/16_8.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <set>
 
 using namespace std;
 
@@ -9,30 +10,35 @@ using namespace std;
 
 int main() {
 	cout.width(8);
-	vector<string> vec1,vec2,temp,vec4;
+	vector<string> vec1,vec2,temp;
+	set<string> friends;
 	string name;
 	cout << "Enter Mat's friends(enter quit to quit):\n";
 	while (cin >> name && name != "quit") {
 		vec1.push_back(name);
 	}
-	cout << "\nMat's friends:\n";
+	cout << "Mat's friends:\n";
 	copy(vec1.begin(),vec1.end(),ostream_iterator<string>(std::cout, " "));
 	cout << endl;
 
-	cout << "\nEnter Pat's friends(enter quit to quit):\n";
+	cout << "Enter Pat's friends(enter quit to quit):\n";
 	while (cin >> name && name != "quit") {
 		vec2.push_back(name);
 	}
-	cout << "\nPat's friends:\n";
+	cout << "Pat's friends:\n";
 	copy(vec2.begin(),vec2.end(),ostream_iterator<string>(std::cout, " "));
 	cout << endl;
 
-	temp.insert(temp.end(),vec1.begin(),vec1.end());
-	temp.insert(temp.end(),vec2.begin(),vec2.end());
-	sort(temp.begin(),temp.end());
-	auto pos = unique(temp.begin(),temp.end());
-	cout << "\nAll friends:\n";
-	copy(temp.begin(),pos,ostream_iterator<string>(std::cout, " "));
+	friends.insert(vec1.begin(),vec1.end());
+	friends.insert(vec2.begin(),vec2.end());
+	copy(friends.begin(),friends.end(),ostream_iterator<string>(cout," "));
+
+//	temp.insert(temp.end(),vec1.begin(),vec1.end());
+//	temp.insert(temp.end(),vec2.begin(),vec2.end());
+//	sort(temp.begin(),temp.end());
+//	auto pos = unique(temp.begin(),temp.end());
+//	cout << "\nAll friends:\n";
+//	copy(temp.begin(),pos,ostream_iterator<string>(cout, " "));
 
 	return 0;
 }
